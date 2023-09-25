fc6568e1e62c076bea9de8f6eab34d7444d86b9f
mhummel-artiso <73578966+mhummel-artiso@users.noreply.github.com>
2023-09-25 15:51:01 +0000

    add new test compiler

---
+++ src/frontend.chatgpt.py
@@ -0,0 +1,60 @@
+from arpeggio import ParserPython,PTNodeVisitor,visit_parse_tree ,Optional, ZeroOrMore, OneOrMore, EOF
+from arpeggio import RegExMatch
+
+def yaml_grammar():
+    def document():
+        return item, ZeroOrMore('\n', item)
+
+    def item():
+        return [scalar, sequence, mapping]
+
+    def sequence():
+        return "-", item
+
+    def mapping():
+        return key, ":", item
+
+    def key():
+        return regex(r'\w+')
+
+    def scalar():
+        return regex(r'[^\n]+')
+
+    def regex(r):
+        return RegExMatch(r)
+
+    def _():
+        return RegExMatch(r'\s*')
+
+    return document
+
+class YamlVisitor(PTNodeVisitor):
+    def visit_document(self, node, children):
+        return children
+
+    def visit_sequence(self, node, children):
+        return {"type": "sequence", "items": children}
+
+    def visit_mapping(self, node, children):
+        return {"type": "mapping", "key": children[0], "value": children[1]}
+
+    def visit_key(self, node, children):
+        return children[0]
+
+    def visit_scalar(self, node, children):
+        return children[0]
+
+    def visit__(self, node, children):
+        return None
+
+def compile_yaml(yaml_text):
+    parser = ParserPython(yaml_grammar,debug=True,skipws=False)
+    parsed_yaml = parser.parse(yaml_text)
+    # for i in parsed_yaml:
+    #     print(i)
+    # return visit_parse_tree(parsed_yaml, YamlVisitor())
+
+if __name__ == "__main__": 
+    yaml_string = open('./src/test.yml', 'r').read()
+    parsed = compile_yaml(yaml_string)
+    print(parsed)
