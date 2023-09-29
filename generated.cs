f88d0245656a0a9c4e07fe6384f2750bfe58897f
[Samuel Kraut] <[skraut@artiso.com]>
2023-09-29 11:23:38 +0200

    delete creating dir from constructor

---
+++ generated.cs
@@ -0,0 +1,80 @@
+public class Configuration
+{
+    public HttpConfiguration Http { get; set; }
+    public class HttpConfiguration
+    {
+        public RoutersConfiguration Routers { get; set; }
+        public class RoutersConfiguration
+        {
+            public ApiConfiguration Api { get; set; }
+            public class ApiConfiguration
+            {
+                public string Rule { get; set; }
+                public string Service { get; set; }
+                public List<string> Middlewares { get; set; }
+                public List<int> Entrypoints { get; set; }
+            }
+            public TlsConfiguration Tls { get; set; }
+            public class TlsConfiguration
+            {
+                public string Certresolver { get; set; }
+            }
+        }
+        public ServicesConfiguration Services { get; set; }
+        public class ServicesConfiguration
+        {
+            public ApiConfiguration Api { get; set; }
+            public class ApiConfiguration
+            {
+                public LoadbalancerConfiguration Loadbalancer { get; set; }
+                public class LoadbalancerConfiguration
+                {
+                    public ServerConfiguration Server { get; set; }
+                    public class ServerConfiguration
+                    {
+                        public int Port { get; set; }
+                    }
+                }
+            }
+        }
+        public MiddlewaresConfiguration Middlewares { get; set; }
+        public class MiddlewaresConfiguration
+        {
+            public ApiAuthConfiguration ApiAuth { get; set; }
+            public class ApiAuthConfiguration
+            {
+                public BasicauthConfiguration Basicauth { get; set; }
+                public class BasicauthConfiguration
+                {
+                    public UsersConfiguration Users { get; set; }
+                    public class UsersConfiguration
+                    {
+                    }
+                }
+                public List<string> Entrypoints { get; set; }
+                public TlsConfiguration Tls { get; set; }
+                public class TlsConfiguration
+                {
+                    public string Certresolver { get; set; }
+                }
+            }
+            public Fail2banConfiguration Fail2ban { get; set; }
+            public class Fail2banConfiguration
+            {
+                public PluginConfiguration Plugin { get; set; }
+                public class PluginConfiguration
+                {
+                    public Fail2banConfiguration Fail2ban { get; set; }
+                    public class Fail2banConfiguration
+                    {
+                        public string LogLevel { get; set; }
+                        public string Bantime { get; set; }
+                        public string Findtime { get; set; }
+                        public double Maxretry { get; set; }
+                        public bool Enabled { get; set; }
+                    }
+                }
+            }
+        }
+    }
+}
