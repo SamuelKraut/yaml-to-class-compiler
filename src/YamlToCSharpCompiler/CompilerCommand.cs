d1c4a939bdea771f5e32e353c9ab61f1940abe6f
[Samuel Kraut] <[skraut@artiso.com]>
2023-09-29 10:01:26 +0200

    formatting

--- src/YamlToCSharpCompiler/CompilerCommand.cs
+++ src/YamlToCSharpCompiler/CompilerCommand.cs
@@ -7,37 +7,48 @@ namespace YamlToCSharpCompiler;
 
 class CompilerCommand : Command<CompilerCommand.Settings>
 {
-    public class Settings:CommandSettings{
-    
-    [CommandArgument(0,"<source>")]
-    [Description("Path to source yml file")]
-    public string SourcePath { get; set; }="";
-    
-    [CommandOption("-o|--output")]
-    [Description("Output file")]
-    [DefaultValue("./GeneratedClasses.cs")]
-    public string TargetPath {get;set;}
-    [CommandOption("--debug")]
-    [Description("Prints debug informations")]
-    public bool Debug {get;set;}
-    public string GetTargetFile(){
-        if(TargetPath.EndsWith(Path.DirectorySeparatorChar))
-            return TargetPath + "GeneratedClasses.cs";
-        if(!TargetPath.EndsWith(".cs"))
-            return TargetPath + ".cs";
-        return TargetPath;
-    }
-    public override ValidationResult Validate()
+    public class Settings : CommandSettings
     {
-        if(!File.Exists(SourcePath))
-            return ValidationResult.Error("Sourec not exits");
-        return ValidationResult.Success();
+        [CommandArgument(0,"<source>")]
+        [Description("Path to source yml file")]
+        public string SourcePath { get; set; } = "";
+
+        [CommandOption("-o|--output")]
+        [Description("Output file")]
+        [DefaultValue("./GeneratedClasses.cs")]
+        public string TargetPath {get;set;}
+
+        [CommandOption("--debug")]
+        [Description("Prints debug informations")]
+        public bool Debug {get;set;}
+
+        public string GetTargetFile()
+        {
+            if (TargetPath.EndsWith(Path.DirectorySeparatorChar))
+            {
+                return TargetPath + "GeneratedClasses.cs";
+            }
+            if (!TargetPath.EndsWith(".cs"))
+            {
+                return TargetPath + ".cs";
+            }
+            return TargetPath;
+        }
+
+        public override ValidationResult Validate()
+        {
+            if (!File.Exists(SourcePath))
+            {
+                return ValidationResult.Error("Source not exits");
+            }
+            return ValidationResult.Success();
+        }
     }
-}
+
     public override int Execute([NotNull] CommandContext context, [NotNull] Settings settings)
     {
-        var c = new Compiler(settings.SourcePath,settings.GetTargetFile(),settings.Debug);
-        c.Compile();
+        var compiler = new Compiler(settings.SourcePath, settings.GetTargetFile(), settings.Debug);
+        compiler.Compile();
         return 0;
     }
 }
